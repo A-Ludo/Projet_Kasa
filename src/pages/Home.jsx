7ab8c815952be062ed6176f72fe6df58c00b9524
AMBLARD Ludovic <ludovic-amblard@hotmail.fr>
2025-01-10 01:27:23 +0100

    ajout de la page home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../../components/Banner/Banner";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+
+	return (
+		<>
+			<Banner />
+			<div className="cards-container">				
+			</div>
+		</>
+	);
+}
