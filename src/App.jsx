509b7b4d7941c998cb391158945213e776b6ece4
AMBLARD Ludovic <ludovic-amblard@hotmail.fr>
2025-01-10 01:25:04 +0100

    Ajout des routes + composants communs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Link} from 'react-router'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import FicheLogement from "./pages/FicheLogement/FicheLogement";
+import Footer from "./components/Footer/Footer";
+import Error from "./pages/Error/Error.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>app</div>
-    </>
+    <div className="App">
+			<Header />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/logement" element={<FicheLogement />} /> 
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
   )
 }
 
